0eab019f0493eb49a917c04481cec97e43bac066
Pedro3148 <pedrohenrique3148@gmail.com>
2024-03-22 10:04:25 -0300

    atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,12 @@
+const a = 2;
+const b = 3;
+let c;
+
+c = a + b;
+console.log(`Resultado da soma entre ${a} e ${b} é ${c}`)
+c = a - b;
+console.log(`Resultado da subtração entre ${a} e ${b} é ${c}`)
+c = a * b;
+console.log(`Resultado da multiplicação entre ${a} e ${b} é ${c}`)
+c = a / b;
+console.log(`Resultado da divisão entre ${a} e ${b} é ${c}`)
